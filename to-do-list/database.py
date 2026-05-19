9373ac3971f0dc97e999089ec87c2a4c6bf33db1
Ayda Karimzadeh <akarimzade257@gmail.com>
2026-05-19 23:34:55 +0330

    changed the structure of my bot

--- to-do-list/database.py
+++ to-do-list/database.py
@@ -6,12 +6,14 @@ BOT_DB = os.environ.get('BOT_DB')
 def init_db():
     conn = sqlite3.connect(BOT_DB)
     cursor = conn.cursor()
+    cursor.execute("PRAGMA foreign_keys = ON")
 
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         user_id INTEGER UNIQUE,
-        user_name TEXT            
+        user_name TEXT,
+        user_state TEXT DEFAULT "none"           
         )
     """)
     
@@ -21,7 +23,7 @@ def init_db():
         user_id INTEGER,
         task TEXT,
         created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-        status BOLEAN DEFAULT FALSE,
+        status INTEGER DEFAULT 0,
         FOREIGN KEY (user_id) REFERENCES users(user_id)
     )
     """)
@@ -42,6 +44,25 @@ def insert_user(user_id, user_name):
     conn.commit()
     conn.close()
 
+def get_user_state(user_id):
+    conn,cursor = connect_to_db()
+    cursor.execute("SELECT user_state FROM users WHERE user_id=?",(user_id,))
+    state = cursor.fetchone()
+    conn.close()
+
+    if state:
+        return state[0]
+    return "none"
+
+def update_user_state(user_id, user_state):
+    conn, cursor = connect_to_db()
+    cursor.execute(
+        "UPDATE users SET user_state = ? WHERE user_id = ?",
+        (user_state, user_id)
+    )
+    conn.commit()
+    conn.close()
+
 def add_task(user_id, task):
     conn , cursor = connect_to_db()
     cursor.execute("INSERT INTO tasks(user_id,task) VALUES (?,?)",(user_id, task))
@@ -50,7 +71,7 @@ def add_task(user_id, task):
 
 def get_list(user_id):
     conn, cursor  = connect_to_db()
-    cursor.execute("SELECT task, created_at FROM tasks WHERE user_id = ?",(user_id,))
+    cursor.execute("SELECT task, id , status FROM tasks WHERE user_id = ?",(user_id,))
     notes = cursor.fetchall()
     conn.close()
     return notes
@@ -62,6 +83,14 @@ def get_stats(user_id):
     conn.close()
     return count_all
 
+def update_task_status(task_id,user_id):
+    conn,cursor = connect_to_db()
+    cursor.execute("UPDATE tasks SET status = NOT status where id = ? AND user_id = ? ", (task_id,user_id))
+    conn.commit()
+    conn.close()
+    
 def del_task(task_id):
     conn,cursor = connect_to_db()
     cursor.execute("DELETE FROM tasks WHERE id = ?",(task_id,))
+    conn.commit()
+    conn.close()
--- to-do-list/main.py
+++ to-do-list/main.py
@@ -14,69 +14,110 @@ bot = telebot.TeleBot(API_TOKEN)
 
 database.init_db()
 
-def add(message):
-    
-    sent = bot.send_message(message.chat.id, "Add a task.")
-    bot.register_next_step_handler(sent,add_task)
+def main_menu():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    add_btn = types.KeyboardButton('Add Task')
+    list_btn = types.KeyboardButton('List Tasks')
+    stats_btn = types.KeyboardButton('Stats')
+    keyboard.add(add_btn,list_btn,stats_btn)
+    return keyboard
 
-def add_task(message):
-    task = message.text
-    user_id = message.from_user.id
-    database.add_task(user_id=user_id, task=task)
-    bot.send_message(message.chat.id, "✅ noted.")
-    
+def create_task_markup(tasks):
+    markup = types.InlineKeyboardMarkup()
+    for name, task_id, status in tasks:
+        icon = "●" if status == 1 else "○"
+        btn = types.InlineKeyboardButton(f"{name} {icon}", callback_data=str(task_id))
+        markup.add(btn)
+    return markup
+
+def add_menu():
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn_added = types.KeyboardButton("Tasks added")
+    btn_menu = types.KeyboardButton("Back")
+    keyboard.add(btn_added,btn_menu)
+    return keyboard
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     database.insert_user(user_id=user_id, user_name=user_name)
-
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    add_btn = types.KeyboardButton('Add Task')
-    list_btn = types.KeyboardButton('List')
-    stats_btn = types.KeyboardButton('Stats')
-    keyboard.add(add_btn,list_btn,stats_btn)
-
+    #agar karbar jadid nabood pyam motefaveti neshan dahad
     bot.send_message(message.chat.id,
-        f"Welcome to To do list bot *{user_name}* .", reply_markup=keyboard)
+        f"Welcome to To do list bot *{user_name}*.\n\n"
+        "Use the buttons below to add tasks, see your list, and view stats."
+        , reply_markup=main_menu(),parse_mode="Markdown")
 
-@bot.message_handler(func= lambda message: message.text in ["Add Task", "List", "Stats"])
-def buttons_handler(message):
-    if message.text == 'Add Task':
-        add(message)
-        
-    elif message.text == 'List':
-        tasks =database.get_list(user_id=message.from_user.id)
+@bot.callback_query_handler(func= lambda call: call.data.isdigit())       
+def callback_handler(call):
+    task_id = int(call.data)
+    user_id = call.from_user.id
+    database.update_task_status(task_id,user_id)
 
+    tasks = database.get_list(user_id=user_id)
+    markup = create_task_markup(tasks)
+
+    try :
+        bot.edit_message_text(
+            chat_id = call.message.chat.id,
+            message_id = call.message.message_id,
+            text = "📋 Your list :",
+            reply_markup = markup
+        )
+    except Exception as e:
+        logger.error(f"Error editing message: {e}")
+
+    bot.answer_callback_query(call.id)
+    
+@bot.message_handler(func=lambda message: True)
+def handle_messages(message):
+    user_id = message.from_user.id
+    user_state = database.get_user_state(user_id)
+    if message.text == "Add Task":
+        database.update_user_state(user_id,"adding task")
+        bot.send_message(message.chat.id,
+                            "Add tasks. when you are done click *Tasks added*.",
+                            reply_markup=add_menu(),
+                            parse_mode="Markdown")
+        return
+    elif user_state == "adding task":
+        if message.text == "Tasks added":
+            database.update_user_state(user_id,"none")
+            bot.send_message(message.chat.id, "All tasks saved.", reply_markup=main_menu())
+
+        elif message.text == "Back":
+            database.update_user_state(user_id,"none")
+            bot.send_message(message.chat.id, "Back to main menu.", reply_markup=main_menu())
+
+        else:
+            database.add_task(user_id=user_id, task=message.text)
+            return
+
+    elif message.text == "List Tasks":
+        tasks = database.get_list(user_id=user_id)
         if not tasks:
             bot.send_message(message.chat.id, "No task found.")
         else:
-            buttons = []
-            markup = types.InlineKeyboardMarkup()
+            markup = create_task_markup(tasks)
+            bot.send_message(message.chat.id, "📋 Your list:", reply_markup=markup)
+        return
 
-            for name, datetime in tasks:
-                btn = types.InlineKeyboardButton(f"{name} ○",callback_data=name)
-                buttons.append(btn)
-
-            for btn in buttons:
-                markup.add(btn)
-
-            bot.send_message(message.chat.id,"📋 Your list:",reply_markup=markup)
-
-        
-    else:
-        stats = database.get_stats(user_id=message.from_user.id)
-
+    # task hay anjam shode va nashode neshan dahad adad ya masalan nemoodar
+    elif message.text == "Stats":
+        stats = database.get_stats(user_id=user_id)
         if not stats:
             bot.send_message(message.chat.id, "No task found.")
-
         else:
-            bot.send_message(message.chat.id,f"You have {stats} tasks waiting to be done.")
-        
-
+            bot.send_message(message.chat.id, f"You have {stats} tasks waiting to be done.")
+        return
     
-@bot.message_handler(func=lambda message: True)
-def unknown(message):
-    bot.send_message(message.chat.id, "🤔")
+    elif message.text == "Back":
+            database.update_user_state(user_id,"none")
+            bot.send_message(message.chat.id, "Back to main menu.", reply_markup=main_menu())
+            return
+    else:
+        bot.send_message(message.chat.id, "🤔")
+
+# ezafe kardan delete
 
 bot.infinity_polling()
